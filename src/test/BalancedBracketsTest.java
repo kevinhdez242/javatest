aa3b53dc256a120143424985b91ccd215262dd2b
Kevin Hernández <59655521+kevinhdez242@users.noreply.github.com>
2024-03-13 17:51:19 +0100

    Update BalancedBracketsTest.java

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,69 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class BalancedBracketsTest {
-
-    //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertTrue(true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void fewerOpeningBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void onlyOneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void fewerClosingBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void closingBracketsFirstReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void openingBracketsLastReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    public void balancedBracketsWithTextBetweenReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void unbalancedBracketsWithTextBetweenReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void nestedBracketsOnTheSameDepthReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]][]]"));
+    }
+
+    @Test
+    public void noBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("a sample text"));
+    }
+
+    @Test
+    public void unbalancedParensAreIgnoredAndReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[())]"));
+    }
 }
